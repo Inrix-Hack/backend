7bda162ee4ae6d47ab960d25ff6159ed31639401
dannyaam9 <dannyaam9@gmail.com>
2023-11-11 14:04:28 -0800

    added api key variable

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from api import getIncident
 
 app = Flask(__name__)
 
+
+@app.route('/incidents')
+def get_incidents():
+    return getIncident()
 
 @app.route('/')
 def hello_world():
